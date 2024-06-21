9322e29d7123d0f25d6ad180f64e7e879031e5af
lisamoser <lisamoser1@hotmail.com>
2024-06-21 19:15:19 +1000

    Added footer with links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+
+      <footer>
+        This project was coded by Lisa Moser and it is open-sourced on{" "}
+        <a href="https://github.com/lisamoser/react-project" target="_blank">
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://react-weather-project-lisamoser.netlify.app/"
+          target="_blank"
+        >
+          {" "}
+          Netlify.{" "}
+        </a>
+      </footer>
     </div>
   );
 }
