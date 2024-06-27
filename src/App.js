034f6f6082790263f56f8f0da6cba97133d3c699
lisamoser <lisamoser1@hotmail.com>
2024-06-27 14:24:46 +1000

    Added Bootstrap to project

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        This project was coded by Lisa Moser and it is open-sourced on{" "}
-        <a href="https://github.com/lisamoser/react-project" target="_blank">
-          GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://react-weather-project-lisamoser.netlify.app/"
-          target="_blank"
-        >
-          {" "}
-          Netlify.{" "}
-        </a>
-      </footer>
+        <footer>
+          This project was coded by Lisa Moser and it is open-sourced on{" "}
+          <a href="https://github.com/lisamoser/react-project" target="_blank">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://react-weather-project-lisamoser.netlify.app/"
+            target="_blank"
+          >
+            {" "}
+            Netlify.{" "}
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
